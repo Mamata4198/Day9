7f2829dfe9c4e4ee434d38ca4b441b82ea9d3c34
Mamata4198 <mamata.bb@terralogic.com>
2020-12-27 21:24:15 +0530

    navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+function Home() {
+    return (
+        <div className="home" >
+       <center> <h1>Welcome to Home page</h1>
+       <h2>This is my first Navbar creation with react icons</h2></center>
+            
+        </div>
+    )
+}
+
+export default Home
